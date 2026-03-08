a856314b69fe775eaf66f6479408000fb98ef121
ahmadisyraf <isyrafmagic@gmail.com>
2026-03-08 16:17:26 +0800

    Add new toJSON() interface

--- src/interface.ts
+++ src/interface.ts
@@ -14,3 +14,32 @@ export interface GetAllCustomerRequest {
 	pagination: PaginationType;
 	sort: SortType;
 }
+
+export type CustomerBase = {
+	firstName: string;
+	lastName: string;
+	email: string;
+	password: string;
+	dynamicEntity: Record<string, any> | undefined;
+	createdAt: string | undefined;
+	updatedAt: string | undefined;
+};
+
+export type CustomerBaseWithoutPassword = Omit<CustomerBase, 'password'>;
+
+/**
+ * @description convert to client object
+ * @returns {CustomerToJSON}
+ */
+export interface CustomerToJSON {
+	/**
+	 * @description return client object with password
+	 * @returns {CustomerBase}
+	 */
+	includePassword(): CustomerBase;
+	/**
+	 * @description return client object without password
+	 * @returns {CustomerBaseWithoutPassword}
+	 */
+	excludePassword(): CustomerBaseWithoutPassword;
+}
